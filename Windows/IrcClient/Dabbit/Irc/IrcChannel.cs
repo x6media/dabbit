7c338ef1e38b75113959fa7450cb80bd3da40d20
David <dabitp@gmail.com>
2013-10-15 08:59:08 -0700

    IRC Channel Changes
    Quick changes to IRC Channel

--- Windows/IrcClient/Dabbit/Irc/IrcChannel.cs
+++ Windows/IrcClient/Dabbit/Irc/IrcChannel.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dabbit.Base;
 
 namespace Dabbit.Irc
 {
-    class IrcChannel
+    public class IrcChannel : IrcEntity
     {
+        public IrcChannel(string channelName)
+            : base(channelName)
+        {
 
+        }
+
+        public 
         private Dictionary<string, object> userList = null;
     }
 }
